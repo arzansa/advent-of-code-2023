ce45e0e8749e04da741bb2e140d65703bb3b2e4f
arzansa <arzansa@gmail.com>
2023-12-03 00:35:38 -0600

    day 1 part 2 solved

--- day-1/part2.py
+++ day-1/part2.py
@@ -17,17 +17,18 @@ replacements = {
 with open('input.txt') as f:
     for line in f:
         print(line)
-        
+
+        # see above comment on dict
         for replacement in replacements.keys():
             line = line.replace(replacement, replacements[replacement])
 
         for dig in range(0, len(str_digits)):
             if str_digits[dig] in line:
                 line = line.replace(str_digits[dig], str(dig+1))
-        print(line)
+        #print(line)
 
         digits = ''.join(x for x in line if x.isdigit())
         sum += int(digits[0] + digits[-1])
-        print(str(int(digits[0] + digits[-1])))
+        #print(str(int(digits[0] + digits[-1])))
 
 print('answer = ' + str(sum))
