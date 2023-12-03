f6b2dcda93165a8ec1460871faa8d1b7e9e877e8
arzansa <arzansa@gmail.com>
2023-12-02 23:35:18 -0600

    day 1 solved

---
+++ day-1/main.py
@@ -0,0 +1,9 @@
+sum = 0
+
+with open('input.txt') as f:
+    for line in f:
+        digits = ''.join(x for x in line if x.isdigit())
+        sum += int(digits[0] + digits[-1])
+        # print(str(int(digits[0] + digits[-1])))
+
+print('answer = ' + str(sum))
