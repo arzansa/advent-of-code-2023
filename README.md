743922b4fac279f38cbadc2b044f6036c6a82292
arzansa <arzansa@gmail.com>
2023-12-05 21:40:39 -0600

    added star table to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # advent-of-code-2023
+| Day | Part 1 | Part 2 |
+| :---: | :---: | :---: |
+| [Day 1](https://adventofcode.com/2023/day/1) | ⭐ | ⭐ |
+| [Day 2](https://adventofcode.com/2023/day/2) | ⭐ | ⭐ |
--- day-2/part2.py
+++ day-2/part2.py
@@ -2,12 +2,17 @@ max_red = 12
 max_green = 13
 max_blue = 14
 sum = 0
+sum_of_powers = 0
 
 with open('input.txt') as f:
 
     for line in f:
 
         possible = True
+        min_red = 0
+        min_green = 0
+        min_blue = 0
+        power_of_mins = 0
 
         cubes = (''.join(i for i in line if i.isdigit() or
                        i in ' ')).split()
@@ -18,23 +23,32 @@ with open('input.txt') as f:
         colors.pop(0)
         
         for i in range(0, len(cubes)):
-                
+
             if colors[i] == 'red':
                 if int(cubes[i]) > max_red:
                     possible = False
+                if int(cubes[i]) > min_red:
+                    min_red = int(cubes[i])
 
             elif colors[i] == 'green':
                 if int(cubes[i]) > max_green:
                     possible = False
+                if int(cubes[i]) > min_green:
+                    min_green = int(cubes[i])
 
             elif colors[i] == 'blue':
                 if int(cubes[i]) > max_blue:
                     possible = False
+                if int(cubes[i]) > min_blue:
+                    min_blue = int(cubes[i])
         
         if (possible):
-            print(f'Game {gamenum} is possible')
+            #print(f'Game {gamenum} is possible')
             sum += int(gamenum)
-        else:
-            print(f'Game {gamenum} is impossible')
+        #else:
+            #print(f'Game {gamenum} is impossible')
+        power_of_mins = min_red * min_green * min_blue
+        sum_of_powers += power_of_mins
 
-print(f'Solution is {sum}')
+print(f'Part 1 solution is {sum}')
+print(f'Part 2 solution is {sum_of_powers}')
